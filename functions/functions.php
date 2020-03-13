b3ce3891f161f93ce30b148da6175fb58473d354
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-03-13 18:46:36 +0100

    Added more project files

---
+++ functions/functions.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Shlinkio\Shlink\Common;
+
+use Laminas\Config\Factory;
+use Laminas\Stdlib\Glob;
+
+/**
+ * Loads configuration files which match provided glob pattern, and returns the merged result as array
+ */
+function loadConfigFromGlob(string $globPattern): array
+{
+    return Factory::fromFiles(Glob::glob($globPattern, Glob::GLOB_BRACE));
+}
