d3afb1b8f7dfb088d2366e7d6437bbc442a6a2bf
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-01-15 13:19:44 +0100

    Created config loader that loads env vars not yet defined

---
+++ test-resources/generated_config.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'foo' => 'not_loaded',
+    'foo2' => [],
+    'BAR' => true,
+    'BAZ' => false,
+    'NUMBER' => 3,
+    'invalid' => [
+        'nested_invalid' => 'string',
+    ],
+    'OVERWRITTEN_ENV' => 'overwrite_value',
+    'ENV_ARRAY' => ['foo', 'bar', 'baz'],
+
+];
