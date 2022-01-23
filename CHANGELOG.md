9d69ff630657b6e8f53e07961153c8df971b03fa
Alejandro Celaya <alejandro@alejandrocelaya.com>
2022-01-23 11:21:56 +0100

    Deprecated checks of regular swoole extension

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,7 +13,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com), and this 
 * Added explicitly enabled composer plugins to composer.json.
 
 ### Deprecated
-* *Nothing*
+* Deprecated check for regular swoole extension.
 
 ### Removed
 * *Nothing*
--- src/Factory/SwooleInstalledFactory.php
+++ src/Factory/SwooleInstalledFactory.php
@@ -12,6 +12,7 @@ class SwooleInstalledFactory
 
     public function __invoke(): callable
     {
+        // TODO regular swoole support is deprecated. Stop checking for it once Shlink v4.0.0 is released
         return static fn (): bool => extension_loaded('openswoole') || extension_loaded('swoole');
     }
 }
