ea563c7e228afb7d3ef5d4d2f8e11b7844653764
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-08-21 12:55:43 +0200

    Created global function to check if swoole is installed

--- functions/functions.php
+++ functions/functions.php
@@ -7,6 +7,7 @@ namespace Shlinkio\Shlink\Config;
 use Laminas\Config\Factory;
 use Laminas\Stdlib\Glob;
 
+use function extension_loaded;
 use function getenv;
 use function implode;
 use function is_array;
@@ -52,4 +53,10 @@ function putNotYetDefinedEnv(string $key, mixed $value): void
         default => (string) $value,
     };
     putenv(sprintf('%s=%s', $key, $normalizedValue));
+}
+
+function swooleIsInstalled(): bool
+{
+    // TODO regular swoole support is deprecated. Stop checking for it once Shlink v4.0.0 is released
+    return extension_loaded('openswoole') || extension_loaded('swoole');
 }
--- src/Factory/SwooleInstalledFactory.php
+++ src/Factory/SwooleInstalledFactory.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shlinkio\Shlink\Config\Factory;
 
-use function extension_loaded;
+use function Shlinkio\Shlink\Config\swooleIsInstalled;
 
 class SwooleInstalledFactory
 {
@@ -12,7 +12,6 @@ class SwooleInstalledFactory
 
     public function __invoke(): callable
     {
-        // TODO regular swoole support is deprecated. Stop checking for it once Shlink v4.0.0 is released
-        return static fn (): bool => extension_loaded('openswoole') || extension_loaded('swoole');
+        return swooleIsInstalled(...);
     }
 }
