aaadd3cc83380a79de06715d8c586c25276f7b0f
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-10-13 12:09:57 +0200

    Expose formatEnvVarValue function that stringifies a value to use as an env var

---
+++ test/Functions/FormatEnvVarValueTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShlinkioTest\Shlink\Config\Functions;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestWith;
+use PHPUnit\Framework\TestCase;
+
+use function Shlinkio\Shlink\Config\formatEnvVarValue;
+
+class FormatEnvVarValueTest extends TestCase
+{
+    #[Test]
+    #[TestWith(['foo', 'foo'])]
+    #[TestWith([35, '35'])]
+    #[TestWith([true, 'true'])]
+    #[TestWith([false, 'false'])]
+    #[TestWith([['abc', 'def', 'foo'], 'abc,def,foo'])]
+    #[TestWith([[5, 7, 55], '5,7,55'])]
+    #[TestWith([null, ''])]
+    public function valuesAreFormattedAsExpected(string|int|bool|array|null $value, string $expectedResult): void
+    {
+        self::assertEquals($expectedResult, formatEnvVarValue($value));
+    }
+}
